3535cc403dfabb923dbfb039fbcfcdaeb77e9b11
Uduak Umanah <eyakenojnr@gmail.com>
2024-08-24 21:02:56 +0100

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const p1 = uploadPhoto();
-  const p2 = createUser();
-
-  return Promise.all([p1, p2]).then((value) => {
-    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-  })
-    .catch(() => { console.log('Signup system offline'); });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => console.log(`${photo.body} ${user.firstName} ${user.lastName}`))
+    .catch(() => console.error('Signup system offline'));
 }
