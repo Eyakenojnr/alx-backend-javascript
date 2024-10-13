b6aef1a4dd2a2c82bee3aa80117a466b7e9c9a27
Uduak Umanah <eyakenojnr@gmail.com>
2024-10-14 00:03:05 +0100

    Returns new `ArrayBuffer` with an `Int8` value at a specific position

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,17 @@
 export default function createInt8TypedArray(length, position, value) {
+  // Creating ArrayBuffer of specified length
+  const buffer = new ArrayBuffer(length);
+  // Creating a DataView to manipulate the buffer
+  const int8 = new DataView(buffer);
+
+  // Throw error if position is out of range
   if (position < 0 || position >= length) {
-    throw Error('Position outside range');
+    throw new Error('Position outside range');
   }
-  const newBuffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(newBuffer, 0, length);
-  int8.set([value], position);
-  return new DataView(newBuffer);
+
+  // Set the Int8 value at the specified position
+  int8.setInt8(position, value);
+
+  // Return the DataView
+  return int8;
 }
