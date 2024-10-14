1b7ee57705cca54ebff7ee2011739b5dbd4cdade
Uduak Umanah <eyakenojnr@gmail.com>
2024-10-14 20:11:32 +0100

    Return a string of all set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,10 @@
-const cleanSet = (set, startString) => {
-  if (startString === undefined || startString.length === 0) {
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
-  return [...set]
-    .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
-    .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
-    .join('-');
-};
 
-export default cleanSet;
+  return [...set]
+    .filter((value) => value.startsWith(startString)) // Filter values that start with startString
+    .map((value) => value.slice(startString.length)) // Remove the startString from the value
+    .join('-'); // Join the remaining parts with '-'
+}
