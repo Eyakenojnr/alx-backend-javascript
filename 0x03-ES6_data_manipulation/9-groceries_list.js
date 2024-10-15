ac7f8e52239858c7a0c972ad8eff4b4e41d32a9a
Uduak Umanah <eyakenojnr@gmail.com>
2024-10-16 00:09:04 +0100

    Map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,16 +1,9 @@
-const groceriesList = () => {
-  const res = new Map();
-  const objetos = {
-    Apples: 10,
-    Tomatoes: 10,
-    Pasta: 1,
-    Rice: 1,
-    Banana: 5,
-  };
-  for (const key of Object.keys(objetos)) {
-    res.set(key, objetos[key]);
-  }
-  return res;
-};
-
-export default groceriesList;
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
